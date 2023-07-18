5d18c8186d7dba2f344fe74a2cef12c7c56446a4
Peter Morrow <petermorrowtech@gmail.com>
2023-07-17 21:23:00 -0700

    Falls back to runserver without use_static_handler

--- statictools/management/commands/dev.py
+++ statictools/management/commands/dev.py
@@ -4,4 +4,7 @@ from django.core.management import call_command
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        call_command('runserver', use_static_handler=False)
+        try:
+            call_command('runserver', use_static_handler=False)
+        except TypeError:
+            call_command('runserver')
